f51b0beb917bd4ec9d94199ed9fcd79fa4f05050
MrTomCat11 <ja.perezf11@gmail.com>
2017-04-11 15:23:05 -0500

    Nueva carpeta de proyecto, no funciona gulp en mi maquina

--- Fundamentos_de_JavaScript/Test/gulpfile.js
+++ Fundamentos_de_JavaScript/Test/gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp');
+var gulp = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('scripts', function () {
 
-});
+  browserify('./src/index.js')
+  .transform(babel)
+  .bundle()
+  .pipe(source('index.js'))
+  .pipe(rename('app.js'))
+  .pipe(gulp.dest('public'));
+})
 
-gulp.task('default', ['scripts']);
+gulp.task('default', function () {
+
+});
