2d378d9790158a556425050e360b02100ae72a2d
MrTomCat11 <ja.perezf11@gmail.com>
2017-04-24 08:40:35 -0500

    front casi terminado, empezamos clases para obtener datos del servidor

--- Definitivo_de_JavaScript/Platzigram/src/homepage/index.js
+++ Definitivo_de_JavaScript/Platzigram/src/homepage/index.js
@@ -14,7 +14,7 @@ page('/', function(ctx, next){
        avatar: 'https://scontent-mia1-2.xx.fbcdn.net/v/t1.0-9/15740961_151129478706735_2819762631189393122_n.jpg?oh=fd82f23fc5e623afc013cd1a1abb76c2&oe=59823283'
      },
      url: 'http://materializecss.com/images/office.jpg',
-     likes: 10,
+     likes: 0,
      liked: false,
      createdAt: new Date()
    },
@@ -24,7 +24,7 @@ page('/', function(ctx, next){
        avatar: 'https://scontent-mia1-2.xx.fbcdn.net/v/t1.0-9/15740961_151129478706735_2819762631189393122_n.jpg?oh=fd82f23fc5e623afc013cd1a1abb76c2&oe=59823283'
      },
      url: 'http://materializecss.com/images/office.jpg',
-     likes: 2,
+     likes: 1,
      liked: true,
      createdAt: new Date().setDate(new Date().getDate() - 10)
    }
---
+++ Definitivo_de_JavaScript/Platzigram/src/translate/es.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'likes': '{ likes, number } me gusta ',
+  'logout': 'Salir',
+  'english': 'Inglés',
+  'spanish': 'Español',
+
+  'signup.subheading': 'Registrate para ver fotos de tus amigos estudiando en Platzi',
+  'signup.facebook': 'Iniciar sesión con facebook',
+  'signup.text': 'Iniciar sesión',
+  'email': 'Correo electronico',
+  'username': 'Nombre de usuario',
+  'fullname': 'Nombre completo',
+  'password': 'Contraseña',
+  'signup.call-to-action': 'Registrate',
+  'signup.have-account': '¿Tienes una cuenta?',
+
+  'signin': 'Entrar',
+  'signin.not-have-account': '¿No tienes una cuenta?',
+
+  'language': 'Idioma'
+}
+
+// 0 me gusta
+// 1 me gusta
